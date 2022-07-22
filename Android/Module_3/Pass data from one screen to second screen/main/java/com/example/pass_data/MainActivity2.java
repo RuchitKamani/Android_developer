5554803c2c972333a9fd77034f105a827d98134d
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-07-22 12:29:53 +0530

    Add files via upload

---
+++ Android/Module_3/Pass data from one screen to second screen/main/java/com/example/pass_data/MainActivity2.java
@@ -0,0 +1,25 @@
+package com.example.pass_data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView t1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        t1=findViewById(R.id.t2);
+
+        Intent i = getIntent();
+        String str = i.getStringExtra("e1");
+        t1.setText(str);
+
+    }
+}
