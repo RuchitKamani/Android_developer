9c37dceaa68d08353455705bf30f268236c74c61
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-07-26 15:34:03 +0530

    Add files via upload

---
+++ Kotlin/login & register page/main/java/com/example/myapplication/register_form.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class register_form : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_form)
+
+
+    }
+}
