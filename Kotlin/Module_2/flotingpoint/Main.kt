bcafa71956773b8205175fc3d952f29fee131b1c
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-09-09 16:12:38 +0530

    Add files via upload

---
+++ Kotlin/Module_2/flotingpoint/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+
+ /*   var u1 = inputevalue()
+    u1.display()*/
+
+  /*  var print = print()
+    print.main()*/
+
+ /*   var i1 = integers()
+    i1.display()*/
+
+    var i1 = flotingpoint()
+    i1.point()
+
+
+}
---
+++ Kotlin/Module_2/flotingpoint/flotingpoint.kt
@@ -0,0 +1,12 @@
+class flotingpoint {
+
+    fun point() {
+
+        val first = 1.5f
+        val second = 2.0f
+
+        val product = first * second
+
+        println("The product is: $product")
+    }
+}
