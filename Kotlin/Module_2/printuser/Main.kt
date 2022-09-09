633ba79889b891c9b18aeca41d0d13e7e9eb303b
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-09-09 16:14:52 +0530

    Add files via upload

---
+++ Kotlin/Module_2/printuser/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+ /*   var u1 = inputevalue()
+    u1.display()*/
+
+    var print = print()
+    print.main()
+
+
+
+}
---
+++ Kotlin/Module_2/printuser/print.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+class print {
+
+    fun main() {
+        val scanner = Scanner(System.`in`)
+
+        print("Enter a number: ")
+        val number = scanner.nextInt()
+
+        println("Entered number: $number")
+    }
+}
