cdc02db09a0e51f8d35dd3cda86f07a96938e520
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-09-09 16:15:24 +0530

    Add files via upload

---
+++ Kotlin/Module_2/Quotient and Remainder/Main.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+
+ /*   var u1 = inputevalue()
+    u1.display()*/
+
+  /*  var print = print()
+    print.main()*/
+
+ /*   var i1 = integers()
+    i1.display()*/
+
+ /*   var i1 = flotingpoint()
+    i1.point()*/
+
+
+var i1 = `Quotient and Remainder`()
+    i1.qp()
+
+}
---
+++ Kotlin/Module_2/Quotient and Remainder/Quotient and Remainder.kt
@@ -0,0 +1,14 @@
+class `Quotient and Remainder` {
+    fun qp() {
+
+        val dividend = 25
+        val divisor = 4
+
+        val quotient = dividend / divisor
+        val remainder = dividend % divisor
+
+        println("Quotient = $quotient")
+        println("Remainder = $remainder")
+    }
+
+}
