423ff0acf550e1b50490e0c48ff139c4cef8f39e
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-09-09 16:09:15 +0530

    Add files via upload

---
+++ Kotlin/Module_2/Add Two Integers/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+ /*   var u1 = inputevalue()
+    u1.display()*/
+
+  /*  var print = print()
+    print.main()*/
+
+    var i1 = integers()
+    i1.display()
+
+
+}
---
+++ Kotlin/Module_2/Add Two Integers/inputevalue.kt
@@ -0,0 +1,12 @@
+class inputevalue {
+
+    fun display()
+    {
+        println("Enter value")
+         var a= readln().toInt()
+
+        println("Your Enter value "+a)
+
+
+    }
+}
