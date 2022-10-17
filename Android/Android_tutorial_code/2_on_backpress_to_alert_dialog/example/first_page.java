a905f42102a14fdfeb845aedbe1066d6ebfe0f5b
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-10-17 11:17:07 +0530

    Add files via upload

---
+++ Android/Android_tutorial_code/2_on_backpress_to_alert_dialog/example/first_page.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import com.example.s_alert_dialog.R;
+
+public class first_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(first_page.this);
+        alert.setTitle("Aru you sure want to exit?");
+        alert.setPositiveButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        alert.setNegativeButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+            }
+        });
+        alert.show();
+    }
+}
