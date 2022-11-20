73089fc55988b30c71891299e577dcd0a50df34c
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-11-20 16:33:32 +0530

    Add files via upload

---
+++ Android/Android_tutorial_code/11_LottieFiles_splash/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.a11_lottiefiles_splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+
+           }
+       },3000);
+    }
+}
