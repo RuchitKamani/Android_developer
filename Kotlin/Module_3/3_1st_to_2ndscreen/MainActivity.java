12091a2f412817573981adfc38dae3d3b9014236
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-11-21 11:14:01 +0530

    Add files via upload

---
+++ Kotlin/Module_3/3_1st_to_2ndscreen/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.a3_1st_to_2ndscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnfirst;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnfirst=findViewById(R.id.btnfirst);
+
+        btnfirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
