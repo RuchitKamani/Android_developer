e5c953dea73c9ac693795ea19db559fb4afd4130
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2022-11-21 12:05:45 +0530

    Add files via upload

---
+++ Kotlin/Module_4/5_weburi/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.a5_weburi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.web);
+        webView.loadUrl("https://www.tops-int.com/");
+    }
+}
