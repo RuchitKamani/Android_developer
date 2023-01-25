a6409e2c38fb44541d8d24f7ca8975ebb321e1ba
Ruchit Kamani <98400362+RuchitKamani@users.noreply.github.com>
2023-01-25 15:17:17 +0530

    Add files via upload

---
+++ CoadingOverload/Editor/multitouch/OnDoubleClickListener.kt
@@ -0,0 +1,6 @@
+package com.CoadingOverload.Editor.multitouch
+
+interface OnDoubleClickListener {
+    fun onPhotoViewDoubleClick(view: PhotoView, entity: MultiTouchEntity)
+    fun onBackgroundDoubleClick()
+}
